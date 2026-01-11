75431f6abaf6247c56b8e292d20b0afa491c1ae2
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-11 00:57:12 -0300

    update: atualizar página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    absolute: "CODEX — Cultura Geek Sem Rótulos",
+    absolute: "LEXARA — Cultura Geek Sem Rótulos",
   },
 
   description:
-    "CODEX é um espaço criado entre amigos para quem vive a cultura geek sem rótulos. Filmes, séries, games, HQs, mangás, histórias, origens e curiosidades analisados com paixão, pesquisa e troca de ideias.",
+    "LEXARA é um espaço criado entre amigos para quem vive a cultura geek sem rótulos. Filmes, séries, games, HQs, mangás, histórias, origens e curiosidades analisados com paixão, pesquisa e troca de ideias.",
 
   keywords: [
-    "codex",
+    "lexara",
     "cultura geek",
     "filmes e séries",
     "games",
@@ -28,33 +28,33 @@ export const metadata: Metadata = {
   ],
 
   alternates: {
-    canonical: "https://codex.com.br/",
+    canonical: "https://lexara.com.br/",
   },
 
   openGraph: {
-    title: "CODEX — Cultura Geek Sem Rótulos",
+    title: "LEXARA — Cultura Geek Sem Rótulos",
     description:
       "Um espaço para quem ama cultura geek sem disputas: filmes, séries, games, HQs, mangás e histórias analisados com critério, curiosidade e paixão.",
-    url: "https://codex.com.br/",
-    siteName: "CODEX",
+    url: "https://lexara.com.br/",
+    siteName: "LEXARA",
     locale: "pt_BR",
     type: "website",
     images: [
       {
-        url: "https://codex.com.br/og-home.png",
+        url: "https://lexara.com.br/og/og-lexarahome.png",
         width: 1200,
         height: 630,
-        alt: "CODEX — Cultura Geek Sem Rótulos",
+        alt: "LEXARA — Cultura Geek Sem Rótulos",
       },
     ],
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "CODEX — Cultura Geek Sem Rótulos",
+    title: "LEXARA — Cultura Geek Sem Rótulos",
     description:
       "Filmes, séries, games, HQs e mangás analisados com paixão, pesquisa e troca de ideias.",
-    images: ["https://codex.com.br/og-home.png"],
+    images: ["https://lexara.com.br/og/og-lexarahome.png"],
   },
 };
 
