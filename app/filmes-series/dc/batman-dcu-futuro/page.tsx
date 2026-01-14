e66e11fb518bccea24c98a996033492c35977a5a
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-14 00:42:43 -0300

    feat: adicionar rota /filmes-series/dc/batman-dcu-futuro

---
+++ app/filmes-series/dc/batman-dcu-futuro/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function BatmanDCUFuturoPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
