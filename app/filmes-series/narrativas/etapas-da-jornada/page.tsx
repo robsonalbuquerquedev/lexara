e443e4f376baf7c88def967599b2262854a6b51c
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-14 21:51:45 -0300

    feat: adicionar rota /filmes-series/narrativas/etapas-da-jornada

---
+++ app/filmes-series/narrativas/etapas-da-jornada/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function EtapasDaJornadaPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
