f4d02c2621f43b213df2d8d44342ff5cbeca5ab6
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-15 15:38:34 -0300

    feat: adicionar rota /games/mecanicas-design/level-design-e-flow

---
+++ app/games/mecanicas-design/level-design-e-flow/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function LevelDesignEFlowPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
