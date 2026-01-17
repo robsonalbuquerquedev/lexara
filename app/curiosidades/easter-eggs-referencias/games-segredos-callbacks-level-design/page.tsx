a4d3885596e52c5271b831c6f64a3bc33328b66b
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 17:56:50 -0300

    feat: adicionar rota /curiosidades/easter-eggs-referencias/games-segredos-callbacks-level-design

---
+++ app/curiosidades/easter-eggs-referencias/games-segredos-callbacks-level-design/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function GamesSegredosCallbacksLevelDesignPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
