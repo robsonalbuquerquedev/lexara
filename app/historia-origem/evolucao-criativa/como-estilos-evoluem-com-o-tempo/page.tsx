75c173bfe3a77e91732c7e00cc3c9a19ae16de28
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 15:46:16 -0300

    feat: adicionar a rota /historia-origem/evolucao-criativa/como-estilos-evoluem-com-o-tempo

---
+++ app/historia-origem/evolucao-criativa/como-estilos-evoluem-com-o-tempo/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function ComoEstilosEvoluemComOTempoPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
