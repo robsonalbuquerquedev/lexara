daf418540bb5bb80a62d27e90e83b5d74fd946bd
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 15:08:46 -0300

    feat: adicionar rota /historia-origem/bastidores-producao/conflitos-de-producao-e-impacto-no-resultado

---
+++ app/historia-origem/bastidores-producao/conflitos-de-producao-e-impacto-no-resultado/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function ConflitosDeProducaoEImpactoNoResultadoPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
