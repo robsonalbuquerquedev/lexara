00895eb3ca657d93fc86dc80ae0cbdeacbf2f7ff
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 15:47:02 -0300

    feat: adicionar a rota /historia-origem/evolucao-criativa/mudancas-de-formato-que-redefiniram-a-experiencia

---
+++ app/historia-origem/evolucao-criativa/mudancas-de-formato-que-redefiniram-a-experiencia/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function MudancasDeFormatoQueRedefiniramAExperienciaPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
