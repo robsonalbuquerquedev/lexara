9f52afb71c85d236404ee49c3ef1c922737fef4b
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-16 21:09:59 -0300

    feat: adicionar rota /hqs-mangas/arcos-classicos-sagas/saga-de-cell

---
+++ app/hqs-mangas/arcos-classicos-sagas/saga-de-cell/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function SagaDeCellPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
