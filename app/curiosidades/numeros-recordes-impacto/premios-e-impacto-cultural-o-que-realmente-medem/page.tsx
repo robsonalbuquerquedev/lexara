e31fbf9d25ab4a8697e40a3debc7280f84048ddc
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 23:05:59 -0300

    feat: adicionar rota /curiosidades/numeros-recordes-impacto/premios-e-impacto-cultural-o-que-realmente-medem

---
+++ app/curiosidades/numeros-recordes-impacto/premios-e-impacto-cultural-o-que-realmente-medem/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function PremiosEImpactoCulturalOQueRealmenteMedemPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
