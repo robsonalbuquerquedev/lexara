37f6a1837045777ed64f05a0fc418666dd9b8c2a
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-18 01:02:05 -0300

    feat: adicionar rota /historia-origem/impacto-cultural-legado/impacto-cultural-marvel

---
+++ app/historia-origem/impacto-cultural-legado/impacto-cultural-marvel/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function ImpactoCulturalMarvelPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
