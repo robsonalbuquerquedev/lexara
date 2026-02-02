773c0e2e8dc8d7bcdcf833e55b5649a7d65fd866
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-02-02 02:29:04 -0300

    feat: adicionar rota /filmes-series/dc/mulher-maravilha/mulher-maravilha-1984

---
+++ app/filmes-series/dc/mulher-maravilha/mulher-maravilha-1984/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function MulherMaravilha1984Page() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
