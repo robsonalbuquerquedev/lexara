3b0a886aa68d326258e9740c8d1a21dcd3f636db
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-02-05 15:44:48 -0300

    update: atualizar rota /filmes-series/marvel/thor

---
+++ app/filmes-series/marvel/thor/page.tsx
@@ -0,0 +1,20 @@
+import CardLayout from "@/components/layout/CardLayout";
+import BackButton from "@/components/navigation/BackButton";
+import ThorCard from "@/components/card/ThorCard";
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function MarvelThorPage() {
+    return (
+        // <CardLayout
+        //     header={
+        //         <BackButton
+        //             fallbackHref="/filmes-series"
+        //             label="Voltar para Filmes & Séries"
+        //         />
+        //     }
+        // >
+        //     <ThorCard />
+        // </CardLayout>
+        <ArticleUnderConstruction />
+    );
+}
