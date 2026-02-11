d036d2df2de36b6a97dafc7cd7a0d00affc5ea95
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-02-11 04:05:35 -0300

    feat: adicionar arquivo capitaoAmericaArticles.ts

---
+++ src/data/marvel/capitao-america/capitaoAmericaArticles.ts
@@ -0,0 +1,52 @@
+export type CapitaoAmericaArticle = {
+    id: number;
+    title: string;
+    excerpt: string;
+    image: string;
+    category: string;
+    href: string;
+    publishedAtISO?: string;
+};
+
+export const capitaoAmericaArticles: CapitaoAmericaArticle[] = [
+    // {
+    //     id: 1,
+    //     title: "Capitão América: O Primeiro Vingador e o nascimento do símbolo",
+    //     excerpt:
+    //         "Criado como propaganda de guerra, Steve Rogers começa como um ideal absoluto. Uma análise de como o MCU transforma um símbolo patriótico em personagem trágico.",
+    //     image: "/images/featured/marvel/capitao-america/capitao-america-primeiro-vingador.png",
+    //     category: "Filmes & Séries",
+    //     href: "/filmes-series/marvel/capitao-america/o-primeiro-vingador-simbolo",
+    //     publishedAtISO: "2026-02-10T09:00:00-03:00",
+    // },
+    // {
+    //     id: 2,
+    //     title: "Entre propaganda e ideal: a culpa histórica do Capitão América",
+    //     excerpt:
+    //         "O escudo carrega mais do que metal: carrega narrativas políticas, guerras e silêncios. Entenda como o MCU confronta o passado ao invés de glorificá-lo.",
+    //     image: "/images/featured/marvel/capitao-america/capitao-america-culpa-historica.png",
+    //     category: "Filmes & Séries",
+    //     href: "/filmes-series/marvel/capitao-america/culpa-historica-e-ideal",
+    //     publishedAtISO: "2026-02-10T09:00:00-03:00",
+    // },
+    // {
+    //     id: 3,
+    //     title: "O Soldado Invernal: quando o herói entra em guerra com o sistema",
+    //     excerpt:
+    //         "Espionagem, paranoia e escolhas morais difíceis. Uma leitura crítica de como O Soldado Invernal redefine o tom político do MCU.",
+    //     image: "/images/featured/marvel/capitao-america/soldado-invernal-critica.png",
+    //     category: "Filmes & Séries",
+    //     href: "/filmes-series/marvel/capitao-america/o-soldado-invernal-analise",
+    //     publishedAtISO: "2026-02-10T09:00:00-03:00",
+    // },
+    // {
+    //     id: 4,
+    //     title: "Guerra Civil: o Capitão América e o preço de fazer a escolha certa",
+    //     excerpt:
+    //         "Quando obedecer deixa de ser moralmente aceitável, o símbolo se parte. Uma análise sobre liderança, lealdade e ruptura no coração do MCU.",
+    //     image: "/images/featured/marvel/capitao-america/guerra-civil-escolhas-morais.png",
+    //     category: "Filmes & Séries",
+    //     href: "/filmes-series/marvel/capitao-america/guerra-civil-escolhas",
+    //     publishedAtISO: "2026-02-10T09:00:00-03:00",
+    // },
+];
