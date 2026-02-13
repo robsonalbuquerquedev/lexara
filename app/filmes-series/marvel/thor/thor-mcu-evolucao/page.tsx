c42a37279b17724ba93de9f89e1a5d9072fcdf68
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-02-13 03:22:44 -0300

    refactor: arquivos renomeados

--- app/filmes-series/marvel/thor/thor-mcu-evolucao/page.tsx
+++
@@ -1,7 +0,0 @@
-import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
-
-export default function ThorMcuEvolucaoPage() {
-    return (
-        <ArticleUnderConstruction />
-    )
-}
