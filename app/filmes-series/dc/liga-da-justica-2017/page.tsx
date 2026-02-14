37571a8acc26e1af59ac168dd98a0febd01e01b4
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-02-14 01:11:35 -0300

    feat: adicionar rota /filmes-series/dc/liga-da-justica-2017

---
+++ app/filmes-series/dc/liga-da-justica-2017/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function LigaDaJustica2017Page() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
